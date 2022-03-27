d63d7ae9022be1fe5dc2dd793367a4f490fd6d17
kartik patel <karishma@ladyboss.com>
2022-03-27 11:09:32 +0530

    scocket connection established

--- src/index.js
+++ src/index.js
@@ -12,8 +12,16 @@ const port = 3000;
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+let count = 0;
+
+io.on('connection', (socket) => {
   console.log('New Scoket connection established');
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count++;
+    io.emit('countUpdated', count);
+  });
 });
 
 server.listen(port, () => {
