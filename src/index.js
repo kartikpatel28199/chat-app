8bd9644189956c388ad404fc51c1527e431d8a88
kartik patel <karishma@ladyboss.com>
2022-03-29 09:03:29 +0530

    heroku port

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicDirectoryPath));
 
